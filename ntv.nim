846cd5c0ade6e27799481d29479c174e52ebf7d9
Lecale <Lecale@users.noreply.github.com>
2020-07-19 10:51:19 +0200

    nirsoft traffic view with cpu and mem polling

--- ntv.nim
+++ ntv.nim
@@ -1,17 +1,63 @@
 import os
 import osproc
 import strutils
+import threadpool
 
 #
 # This program makes use of this utility
 # https://www.nirsoft.net/utils/network_traffic_view.html
+# The exe file needs to be run in Admin mode
 #
 
-echo "nimMgr"
+proc taskList(a: string, b: int): int =
+  let posnM: int = 4
+  let posnC: int = 8
+  var t:string = "tasklist /FI \"IMAGENAME eq " & a & "\" /NH /v"
+  var o = execProcess(t).splitLines()
+  var mem: int = 0
+  var timeUnit: int = 0
+
+  for ol in o:
+    var p = split(ol)
+    var pi:seq[string] = @[]
+    # clean data
+    for pl in p:
+      let fudge:string = (string) pl.replace(",","")
+      if(fudge.isNilOrWhitespace == false):
+        pi.add(fudge)
+    # easy to add the Kb
+    try:
+      mem = mem + parseInt(pi[posnM])
+    except:
+      mem = mem + 0
+    # CPUTIME is in hours:minutes:seconds
+    try:
+      var tu = split(pi[posnC],":")
+      timeUnit = timeUnit + parseInt(tu[2])
+      timeUnit = timeUnit + 60 * parseInt(tu[1])
+      timeUnit = timeUnit + 3600 * parseInt(tu[0])
+    except:
+      discard
+
+  let a:string = "" & $(mem/1024) & "\t" & $timeUnit & "\t#" & $b
+  let f = open("rc.txt", fmAppend)
+  defer: f.close()
+  f.writeLine(a)
+
+############################################################
+
+proc bw(win: int): int =
+  let ntv: string = "NetworkTrafficView.exe /shtml \"record.html\" /sort \"Data Speed\" /CaptureTime " & $win
+# nirsoft's exe file should have a config change to group by process name
+  discard execProcess(ntv)
+  return 0
+############################################################
+
+
+echo "nim resource consumption utility"
 var pNom: string = "chrome.exe"
 var bwWindow: int = 60
-let posnM: int = 4
-let posnC: int = 8
+let nap:int = (int)(100 * bwWindow)
 try:
   pNom = paramStr(1)
 except:
@@ -20,39 +66,10 @@ try:
   bwWindow = parseInt(paramStr(2))
 except:
   echo "nimMgr [processName] [window_size]"
-  
-var t:string = "tasklist /FI \"IMAGENAME eq " & pNom & "\" /NH /v"
-# with /v we can add extra params like CPUTIME and window title 
-var ntv: string = "NetworkTrafficView.exe /shtml \"record.html\" /sort \"Data Speed\" /CaptureTime " & $bwWindow
-#   "
-discard execProcess(ntv)
-var o = execProcess(t).splitLines()
-var mem: int = 0
-var timeUnit: int = 0
 
-for ol in o:
-#  echo ol
-  var p = split(ol)
-  var pi:seq[string] = @[]
-  # clean data
-  for pl in p:
-    let fudge:string = (string) pl.replace(",","")
-    if(fudge.isNilOrWhitespace == false):
-      pi.add(fudge)
-  # easy to add the Kb
-  try:
-    mem = mem + parseInt(pi[posnM])
-  except:
-    mem = mem + 0
-  # CPUTIME is in hours:minutes:seconds
-  try:
-    var tu = split(pi[posnC],":")
-    timeUnit = timeUnit + parseInt(tu[2])
-    timeUnit = timeUnit + 60 * parseInt(tu[1])
-    timeUnit = timeUnit + 3600 * parseInt(tu[0])
-  except:
-    echo "handled exception"
-echo "Memory for process group:\t" , mem/1024 , "\tMb" 
-echo "Time Units for process group:\t" , timeUnit 
-
-# discard execShellCmd("ls -la")
+var f1 = spawn bw(bwWindow)
+var cnt:int = 0
+while not f1.isReady:
+  discard spawn taskList(pNom,cnt)
+  cnt = cnt + 1
+  sleep(nap)
